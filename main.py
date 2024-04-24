efb6566629d7c1eceba85a51e8607c25e39bc896
Adasinho <adirm10@yahoo.com>
2024-04-24 20:23:58 +0200

    Remodel logs time format

--- main.py
+++ main.py
@@ -94,8 +94,8 @@ serviceParam = args.service
 # logging configuration
 logging.basicConfig(filename=LOG_FILE_NAME,
                     filemode='a',
-                    format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
-                    datefmt='%H:%M:%S',
+                    format='%(asctime)s - %(levelname)s - %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S',
                     level=logging.DEBUG)
 
 restartCounter = 0
